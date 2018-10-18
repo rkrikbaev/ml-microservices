65fb4167ad26d0f8b888debfe92a6b99bef7691f
rkrikbaev <rkrikbaev@gmail.com>
2018-10-18 18:00:50 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,43 @@ from socket import *
 import json, os
 import time
 from datetime import tzinfo, timedelta, datetime
-
+import argparse
 import link as lk
 import handler as mh
+import config as cf
+
 import _thread
 
 def loadConfig(**kwargs):
 
-    source_conf, model_conf, store_conf = lk.Connector.getdata()
+    config = cf.Config()
+
+    source_conf, model_conf, store_conf = config.readModelsConfigFile(file='./model.conf')
 
     return source_conf, model_conf, store_conf
 
-def workflow(**kwargs):
+def workflow(id, source_conf, model_conf, store_conf, **kwargs):
 
-    link = lk.Connector()
-    model = mh.Model()
+    flowid = str(id)
+    
+
+    linktodata = lk.Connector(id)
+    model = mh.Model(flowid)
 
     source_conf, model_conf, store_conf = loadConfig()
-    data = link.getdata(source_conf, model_conf, store_conf)
-    data = model.query(data)
+    data = linktodata. .getdata(source_conf)
+    data = model.query(data, model_conf)
     link.writedata(store_conf, data)
 
 def main():
     print("start")
+
+    loadConfig()
+
     try:
-        _thread.start_new(workflow(),)
-        _thread.start_new(workflow(),)
+        _thread.start_new(workflow(a[0]),)
+        _thread.start_new(workflow(a[1],)
+
 
 if __name__ == '__main__':
     main()
